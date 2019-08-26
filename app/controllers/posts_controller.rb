57be23fbb4fe5664d0b0fc75e26b0cada7a3d906
Jeremiah Rodden <jrodden1@gmail.com>
2019-08-26 13:09:54 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,25 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.comments.build
+    @post.users.build
+  end
+
+  def update
+    @post = Post.find_by_id(params[:post][:post_id])
+    
+    # if params[:comment][:user_id].empty? 
+    #   @user_id = User.create(username: params[:comment][:user]).id
+    #   params[:comment][:user_id] = @user_id
+    # end
+    binding.pry
+    @post.comments.build(comment_params)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+    
   end
 
   def create
@@ -21,4 +40,9 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :comment_user_attributes_username)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@user1 = User.create(username: "User1", email: "user1@example.com")
+@user2 = User.create(username: "User2", email: "user2@example.com")
+
+@category1 = Category.create(name: "Feelings")
+@category2 = Category.create(name: "Thoughts")
+@category3 = Category.create(name: "Theories")
+
+@post1 = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+@post2 = Post.create(title: "Feeling Extra Groovy", content: "I'm feeling extra groovy")
+@post3 = Post.create(title: "Feeling Groggy", content: "I'm feeling so groggy")
+
+@post1.categories << @category1
+@post1.categories << @category3
+@post2.categories << @category1
+
+@user1.posts << @post1 
+@user1.posts << @post3 
+@user2.posts << @post2
+
+@comment1 = Comment.create(content: "So Groovy, man!")
+@comment1.user = @user2
+@post1.comments << @comment1
+
