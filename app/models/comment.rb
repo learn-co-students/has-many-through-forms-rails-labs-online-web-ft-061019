f733d77ded9218edc958c2a83fdb5b14bf7aa12f
Jeremiah Rodden <jrodden1@gmail.com>
2019-08-26 13:33:29 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,16 +2,16 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user, reject_if: :all_blank?
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
-  def comment_user_attributes_username=(username)
-    binding.pry
-    if self.user_id == ""  
-      @user = User.create(username)
-      self.user_id = @user.id
-      self.save
-    end
-  end
+  # def comment_user_attributes_username=(username)
+    # binding.pry
+    # if self.user_id == ""  
+      # @user = User.create(username)
+      # self.user_id = @user.id
+      # self.save
+    # end
+  # end
 
   # def comment_user_attributes_username
   #   User.find_by_id(self.id)
